1541db2a552c8f6dde9c1a9656e9834b9624b9c0
Gevorg9090 <ghambaryangevorg99@gmail.com>
2025-03-24 22:13:45 +0400

    my first homework

--- src/Gevorg/App.jsx
+++ src/Gevorg/App.jsx
@@ -1,13 +1,41 @@
 import { useState } from "react";
 import React from "react";
+import './App.css';
 
 function App() {
-  const [name, setname] = useState("Ani")
+  const [image] = useState([
+    {
+     image: "./images/audi.avif",
+      name: "Audi",
+      price: 45456
+    },
+    {
+      image: "./images/bmw.avif",
+      name: "BMW",
+      price: 58700
+    }, 
+    {
+      image: "./images/mercedes benz.avif",
+      name: "Mercedes Benz",
+      price: 49500
+    }
+  ]);
 
-  return <div className='App'>
-    <h1>{name}</h1>
-    <button onClick={setname}></button>
-  </div>;
+  return <div className="box">
+    <h1>Cost in stoc</h1>
+  
+   {
+     image.map(elem => {
+      return (
+        <div className="child">
+          <img src={elem.image} alt={elem.name}/>
+          <h2>{elem.name}</h2>
+          <h3>Price: {elem.price}</h3>
+        </div> 
+      )
+    })
+   }
+  
+  </div>
 }
-
 export default App;

83a3352b2867e66a000d6e1e3eefd68c2f31c5c5
BrownEdgar <43911617+BrownEdgar@users.noreply.github.com>
2025-03-27 19:14:50 +0400

    clear new develop branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import { useState } from 'react';
-import List from './components/List/List';
-
-import './App.css'
+import './App.scss'
 
 function App() {
-  const [cities, setCities] = useState(['Vanadzor', "Yerevan", "Gyumri"]);
-
-  function deleteCity() {
-    // setCities(cities.slice(0, -1))
-    setCities(cities.toSpliced(-1, 1))
-  }
-
-  return (
-    <div className='App'>
-      <h1 className="title">React List</h1>
-      <List data={cities} deleteCity={deleteCity} />
-    </div>
-  )
+  return <div className='App'></div>;
 }
 
 export default App
-
-
-
-

70a4aea851cf7625d2cd66b59118be93a827936e
BrownEdgar <43911617+BrownEdgar@users.noreply.github.com>
2025-04-05 19:03:29 +0400

    react Form

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './App.css'
-import App from './components/hooks/CustomHooks/App'
+import App from './components/ReactForm/App'
 
 createRoot(document.getElementById('root')).render(<App />)
